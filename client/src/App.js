364edc8c441b65445cdf219ceac068d98088884a
Justin Garner <j.garner828@gmail.com>
2022-05-05 14:08:39 -0400

    starting new portfolio design

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import {Container} from './components/styles/Container.styled'
+import React from 'react';
+
+function App() {
+  return (
+    <Container>
+        <h1>Test version of new Portfolio</h1>
+    </Container>
+  );
+}
+
+export default App;
---
+++ client/src/components/styles/Container.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    width: 1000vw;
+    height: 100vh;
+    padding: 0 20px;
+    margin: 0 auto;
+    background: red;
+    `
